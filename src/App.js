08430a3d29d70afc59816493e63d432217c7e2e5
山西　一正 <addas.baboon@gmail.com>
2018-09-29 13:08:44 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div><h1>Hello, World!</h1></div>;
-  }
+// class App extends Component {
+//   render() {
+//     return <h1>Hello, World!</h1>;
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
